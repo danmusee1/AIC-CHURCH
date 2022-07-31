3bdd999a49f9e7f2e67f97d0eaf6d49b578ac62e
rayzeydmusee <dmusee721@gmail.com>
2022-07-31 15:55:45 +0300

    into page creation and json data file

---
+++ components/Intro.js
@@ -0,0 +1,39 @@
+
+import Navbar from './Navbar'
+
+export default function Intro() {
+  return (
+    <div>
+    <div className=" ul bg-black bg-opacity-0 lg:p-10 w-full h-full hover:bg-opacity-50 transition-all duration-1000 bg-cover bg-center bg-[url(https://352zl3r81ot2rnkzo1r0n6uj-wpengine.netdna-ssl.com/wp-content/uploads/2021/06/volunteer-bg-01.jpeg)]  dark:bg-slate-900  md:max-h-screen" >
+        <div className=" lg:grid  md: gap-20 lg:gap-20 xl:justify-start mt-auto ">
+        <Navbar />
+        <div className=" px-1 lg:px-10   gap-1 lg:gap-4 flex flex-col space-y-1 lg:space-y-4   ">
+        <div>
+            <h1 className='text-5xl lg:text-6xl font-bold lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white color: rgb(0 0 0); link link-underline link-underline-black  items-center justify-center   '> 
+            Discover God.
+            </h1>
+        </div>
+        <div>
+            <h1 className='text-2xl lg:text-2xl lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white color: rgb(0 0 0); link link-underline link-underline-black  items-center justify-center   '>
+                Join us Sundays online, or visit our AIC Area<br></br> location.
+                </h1>
+        </div>
+        <div className=" py-7 px-2 space-x-2 lg:space-x-3 lg:flex" >
+            <div> 
+                <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 inline-block px-7 py-3 bg-gray-200  font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">view location</button>
+                </div>
+           <div>
+           <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 inline-block px-6 py-2 border-2 border-gray-100 text-gray-50 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">watch Live stream </button>
+           </div>
+            
+        </div>
+        </div>
+        
+        </div>
+        
+    </div>
+    
+    </div>
+    
+  )
+}
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,28 +3,102 @@
 import {  HomeIcon } from '@heroicons/react/solid'
 import { BellIcon,DotsHorizontalIcon, BookmarkIcon, ClipboardIcon, DotsCircleHorizontalIcon, HashtagIcon, InboxInIcon, UserCircleIcon} from '@heroicons/react/outline'
 import 'tailwindcss/tailwind.css'
+import Link from 'next/link';
+import { useState } from 'react';
 function Navbar() {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
   return (
-    <div className='hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24'>
-        {/*Twitterlogo*/}
-       
-    {/*menu*/}
-    
-     {/*button*/}
-        <button className='bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline '>Tweet</button>
-         {/*min9i-prifile*/}
-       <div  className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto" >
-        <img className='h-10 w-10 rounded-full xl:mr-2'
-       
-         src='https://imgs.search.brave.com/sQF-4cufXFk2wnJpcrER7MdgLKPTHL-oTdLW9AAqHHM/rs:fit:600:600:1/g:ce/aHR0cHM6Ly8xLmJw/LmJsb2dzcG90LmNv/bS8tQWxzcTlCbnY2/N2svVkE0T25aazEz/Q0kvQUFBQUFBQUFE/dWcvV2FDb25VUlVn/OE0vczE2MDAvUm91/bmQlMkJQcm9maWxl/JTJCUGljLnBuZw'
-         alt="user-img"
-        />
-        <div className="leading-5 hidden xl:inline ">
-            <h4 className="font-bold">Dan Musee</h4>
-            <p className='text-gray-500'>@rayzrmd</p>
+    <div className="py-0">
+       <nav className='px-1 lg:py-1 lg:space-x-10 md:px-1  lg:px-10 flex items-center flex-wrap  background-color: transparent; '>
+       <Link href='/'>
+          <a className='inline-flex items-center p-2 mr-4 '>
+          <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+            <img
+             // onClick={signOut}
+              src="https://scontent.fnbo2-1.fna.fbcdn.net/v/t1.6435-9/90653670_102233064759784_7030769173294743552_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=mCc8G1byi1YAX8D6oXy&_nc_ht=scontent.fnbo2-1.fna&oh=00_AT_r8rvhZIYgQpQsLHvA1bDcScb1uepyOPN9CAwoNnOaRw&oe=630A8A61"
+              alt="church-img"
+              className="h-10 w-10 rounded-full xl:mr-2"
+            />
+            </div>
+           
+            <span className='text-xl color: rgb(0 0 0); font-bold text-white uppercase tracking-wide'>
+              AIC UMOJA
+            </span>
+          </a>
+        </Link>
+        <button
+          className=' inline-flex p-3 hover:bg-green-600 rounded lg:hidden color: rgb(0 0 0); ml-auto hover:color: rgb(0 0 0); outline-none'
+          onClick={handleClick}
+        >
+          <svg
+            className='w-6 h-6'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M4 6h16M4 12h16M4 18h16'
+            />
+          </svg>
+        </button>
+        {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+        <div
+          className={`${
+            active ? '' : 'hidden'
+          }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        >
+          <div className=' lg:inline-flex lg:flex-row lg:ml-center lg:w-center w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white color: rgb(0 0 0); link link-underline link-underline-black  items-center justify-center   '>
+              I’M NEW
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white color: rgb(0 0 0); link link-underline link-underline-black  items-center justify-center   '>
+              LOCATIONS
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white color: rgb(0 0 0); link link-underline link-underline-black  items-center justify-center   '>
+              LIVE
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white color: rgb(0 0 0); link link-underline link-underline-black  items-center justify-center   '>
+              LET’S TALK NIGHTS
+                
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white color: rgb(0 0 0); link link-underline link-underline-black  items-center justify-center   '>
+              RESOURCES
+                
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white color: rgb(0 0 0); link link-underline link-underline-black  items-center justify-center   '>
+              GIVE
+                
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white color: rgb(0 0 0); link link-underline link-underline-black  items-center justify-center   '>
+              EVENTS
+                
+              </a>
+            </Link>
+          </div>
         </div>
-        <DotsHorizontalIcon  className='h-5 xl:ml-8 hidden xl:inline'/>
-         </div>
+      </nav>
+   
          </div>
   
 )}
