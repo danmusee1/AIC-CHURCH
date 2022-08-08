2de3f07333312a5a97ffb51f51686c34659c9b9b
rayzeydmusee <dmusee721@gmail.com>
2022-08-08 15:10:20 +0300

    DEPLOY CHANGE

--- components/Newcommer.js
+++ components/Newcommer.js
@@ -9,7 +9,7 @@ export default function Newcommer() {
         <div className="pt-20 grid justify-items-center"> 
         <div className="grid pb-5 lg:pb-10 justify-items-center">
           <h1 className="  text-white-900 text-2xl lg:text-4xl font-bold mb-2">Not sure where to begin?</h1>
-          <h1 className=" text-white-900 text-4xl font-bold mb-2"> <a class="underline decoration-8 decoration-gray-500/90">Start here.</a> </h1>
+          <h1 className=" text-white-900 text-4xl font-bold mb-2"> <a className="underline decoration-8 decoration-gray-500/90">Start here.</a> </h1>
           </div>
           <h1 className="text-white-700 text-lg ">Visit our First Steps page to get an overview of all the ways to </h1>
           <h1 className="text-white-700 text-lg mb-4">get involved with the AIC.</h1>
