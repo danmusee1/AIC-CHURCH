d4175a38d6ae9946ce1563d621fbfc9bcde4ca5d
rayzeydmusee <dmusee721@gmail.com>
2022-09-17 15:16:35 +0300

    error mid

--- components/SpecialContribution/MidBackground.js
+++ components/SpecialContribution/MidBackground.js
@@ -10,7 +10,7 @@ export default function MidBackground() {
         <div id='storytellingmid_background'className=' scroll shapedividers_com-4494 '>
           <div className='lg:flex lg:pl-20  lg:pt-40'>
             <div className="lg:w-1/2 lg:pr-20">
-            <h1 className='pb-5 pt-10 font-bold text-lg text-gray-500'>
+            <h1 className='pb-5 pt-10 font-bold text-lg text-gray-400'>
                 
                 Why we give
                 </h1>
@@ -23,9 +23,9 @@ export default function MidBackground() {
                  It also gives us a chance to build our faith about investing in Gods Kingdom.
                     </h1>
             </div>
-            <div className="">
-              <Image className="rounded-lg" src="https://352zl3r81ot2rnkzo1r0n6uj-wpengine.netdna-ssl.com/wp-content/uploads/2022/07/about-01b.jpeg " alt="image" height=" 900px" width="900px"/>
-            </div>
+            <div className="pr-5 pb-5 pl-5">
+        <Image src="https://media.gettyimages.com/photos/detail-of-the-youtube-logo-outside-the-youtube-space-studios-in-on-picture-id1149449091" alt="image" height="1251%" width="2050%"/>
+      </div>
          
           
           </div>
