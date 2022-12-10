6edb9259d805774eb1e92d686153e645498958b0
danmusee1 <dmusee721@gmail.com>
2022-12-10 09:59:24 +0300

    pastors and team customization

--- components/pastoral team/Pastorals.js
+++ components/pastoral team/Pastorals.js
@@ -20,6 +20,7 @@ export const Pastorals = () => {
     },[]);
     return(
         <div className="flex-col my-10">
+             
            
             {
                 pastorals.length > 0 && (
@@ -27,11 +28,22 @@ export const Pastorals = () => {
                         <div className='grid md:grid-cols-4 grid-cols-1 gap-5'>
                             {
                                 pastorals.map((pastoral) =>(
-                                    <div key={pastoral.id} className='md:mx-0 mx-5 w-72 h-60 rounded-lg'>
-                                        <Image key={pastoral.id} src={pastoral.thumbnailUrl} alt="" width='70' height='70'/>
-                                        <h3 key={pastoral.id}>{pastoral.name}</h3>
-                                        <h3 key={pastoral.id}>{pastoral.email}</h3>
-                                        <h3 key={pastoral.id}>{pastoral.phone}</h3>
+                                    <div key={pastoral.id} className='md:mx-0 mx-5 w-72 h-60 rounded-lg w-64 mx-auto bg-[#20354b] rounded-2xl px-8 py-6 shadow-lg mb-10'>
+                                        <div class="flex items-center justify-between">
+            <span className="text-gray-400 text-sm">position</span>
+            <span className="text-emerald-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+              </svg>
+            </span>
+        </div>
+                                <div class="mt-6 w-fit mx-auto ">
+                                        <Image className="rounded-full w-28 " key={pastoral.id} src="https://api.lorem.space/image/face?w=120&h=120&hash=bart89fe" alt="" width='70' height='70'/>
+                                        </div>
+                                        <h3 className="text-white font-bold text-2xl tracking-wide" key={pastoral.id}>{pastoral.name}</h3>
+                                        <h3 className='text-white' key={pastoral.id}>{pastoral.email}</h3>
+                                        <h3 className='text-white ' key={pastoral.id}>{pastoral.phone}</h3>
+                                        
                                         </div>
                                 ))
                             }
@@ -41,8 +53,8 @@ export const Pastorals = () => {
                 )
             }
              <Link href="teamMain">
-             <div className='ml-60'>
-            <button className=" active:shadow-lg transition duration-150 ease-in-out transition ease-in-out delay-150 px-20 py-6 text-white rounded-xl transition-all duration-500 bg-gradient-to-tl to-white via-black from-red-500 bg-size-200 bg-pos-0 hover:bg-pos-100">view enire team</button>
+             <div className='pt-16 lg:ml-60 ml-5 '>
+            <button className=" active:shadow-lg transition duration-150 ease-in-out transition ease-in-out delay-150 px-24 py-6 text-white rounded-xl transition-all duration-500 bg-gradient-to-tl to-white via-black from-red-500 bg-size-200 bg-pos-0 hover:bg-pos-100">view enire team</button>
             </div>
             </Link>
         </div>
--- components/pastoral team/TeamMembers.js
+++ components/pastoral team/TeamMembers.js
@@ -26,12 +26,23 @@ export const TeamMembers = () => {
                     <div className="flex justify-center items-center">
                         <div className='grid md:grid-cols-4 grid-cols-1 gap-5'>
                             {
-                                teamMembers.map((teamMembers) =>(
-                                    <div key={teamMembers.id} className='md:mx-0 mx-5 w-72 h-60 rounded-lg'>
-                                        <Image key={teamMembers.id} src={teamMembers.thumbnailUrl} alt="" width='70' height='70'/>
-                                        <h3 key={teamMembers.id}>{teamMembers.name}</h3>
-                                        <h3 key={teamMembers.id}>{teamMembers.email}</h3>
-                                        <h3 key={teamMembers.id}>{teamMembers.phone}</h3>
+                                 teamMembers.map((teamMember) =>(
+                                    <div key={teamMember.id} className='md:mx-0 mx-5 w-72 h-60 rounded-lg w-64 mx-auto bg-[#20354b] rounded-2xl px-8 py-6 shadow-lg mb-10'>
+                                        <div class="flex items-center justify-between">
+            <span className="text-gray-400 text-sm">position</span>
+            <span className="text-emerald-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+              </svg>
+            </span>
+        </div>
+                                <div class="mt-6 w-fit mx-auto ">
+                                        <Image className="rounded-full w-28 " key={teamMember.id} src="https://api.lorem.space/image/face?w=120&h=120&hash=bart89fe" alt="" width='70' height='70'/>
+                                        </div>
+                                        <h3 className="text-white font-bold text-2xl tracking-wide" key={teamMember.id}>{teamMember.name}</h3>
+                                        <h3 className='text-white' key={teamMember.id}>{teamMember.email}</h3>
+                                        <h3 className='text-white ' key={teamMember.id}>{teamMember.phone}</h3>
+                                        
                                         </div>
                                 ))
                             }
