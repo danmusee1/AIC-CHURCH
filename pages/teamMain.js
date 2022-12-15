d81ec843ee8dc662439a0de7fb525cd547f5488d
danmusee1 <dmusee721@gmail.com>
2022-12-16 02:52:14 +0300

    members form

--- pages/teamMain.js
+++ pages/teamMain.js
@@ -3,6 +3,7 @@ import TeamMemBackground from '../components/pastoral team/TeamMemBackground'
 import { TeamMembers } from '../components/pastoral team/TeamMembers'
 import Footer from '../components/reusable/Footer'
 import Navbar1 from '../components/reusable/Navbar1'
+import Form from '../components/live/Form'
 
 export default function teamMain() {
   return (
@@ -10,6 +11,7 @@ export default function teamMain() {
         <Navbar1/>
         <TeamMemBackground/>
         <TeamMembers/>
+        <Form/>
         <Footer/>
     </div>
   )
