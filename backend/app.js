3a72742caa6d305d5e24614f89524e1c2392aa9d
danmusee1 <dmusee721@gmail.com>
2022-12-20 02:59:15 +0300

    graphQL installation and set up

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,22 @@
-const express=require("express");
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = { hello: () => 'Hello world!' };
 const app= express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  }));
 const cors= require("cors");
 app.use(cors());
 app.use(express.json());
